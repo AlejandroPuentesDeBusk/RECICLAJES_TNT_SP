6f631835d971a923da3b1b41f37ebbfea06734ec
AlejandroPuentesDeBusk <puentesalejandro12@outlook.com>
2024-11-02 22:00:07 -0400

    avances_compra_venta

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth import get_user_model
+from.models import Material
 
 Users = get_user_model()
 
@@ -19,27 +20,28 @@ def index(request):
 #request de Alex
 
 def compra_1(request):
-    return render(request, 'compra_venta/compra.html')
+    materials = Material.objects.all()
+    return render(request, 'compra_venta/compra.html', {'materials':materials})
 
-@login_required
+#@login_required
 def venta_1(request):
     return render(request, 'compra_venta/venta.html')
 
-@login_required
+#@login_required
 def ajustes_1(request):
     return render(request, 'ajustes_caja/ajustes_1.html')
 
 #request de francisco
-@login_required
+#@login_required
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
 
 #request de Adri
-@login_required
-@user_passes_test(is_owner) 
+#@login_required
+#@user_passes_test(is_owner) 
 def panel_control(request):
     return render(request, 'panel_control/panel.html')
 
-@login_required
+#@login_required
 def pantalla_carga(request):
     return render(request, 'p_carga.html')
