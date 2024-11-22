a510018f359826863995d8dda4114a21ddeee35b
Diego <diego.sierra.alcala@gmail.com>
2024-11-21 20:41:31 -0600

    Cortes_caja V.2

---
+++ app_CorteCaja/static/ajustes_caja/js/caja.js
@@ -0,0 +1,4 @@
+function mostrarSeleccion(tipoTransaccion) {
+    // Obtener el campo de texto por su ID y asignarle el valor de la opción seleccionada
+    document.getElementById('transaccion_seleccionada').value = tipoTransaccion;
+  }
