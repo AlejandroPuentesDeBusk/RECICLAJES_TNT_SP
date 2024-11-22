d62685378b25a5e9d8940ec5dda6e0d8eb3155a0
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2024-11-21 23:30:22 -0600

    DJ buscador( ^u<)^3

--- panel_control/views.py
+++ panel_control/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from mainapp import views
 from django.contrib.auth import get_user_model
 from mainapp.models import Material
+from django.core.paginator import Paginator
 
 Users = get_user_model()
 
@@ -51,3 +52,25 @@ def is_owner(users):
 
 def is_employee(users):
     return users.is_active 
+
+def materiales(request):
+    material_list = Material.objects.all()  # Obtener todos los materiales de la mendiga tabla
+    paginator = Paginator(material_list, 5)  # 5 materiales por página es horrible tener muchas
+    page_number = request.GET.get('page')  # Obtener el número de página desde la URL es un dolor de huevos
+    page_obj = paginator.get_page(page_number)  # Obtener la página solicitada que jode
+    search_query = request.GET.get('search','')
+    # Filtrar los materiales según el término de búsqueda
+    if search_query:
+        materials_list = Material.objects.filter(Material_Type__icontains=search_query)
+    else:
+        materials_list = Material.objects.all()
+    
+    # Configurar la paginación
+    paginator = Paginator(materials_list, 5)  # 5 materiales por página
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'materiales.html', {
+        'page_obj': page_obj,
+        'search_query': search_query
+        })
