6d407d30d438f5991652c47dd0a074f54369f980
Alejandro Puentes de busk <alejandro@192.168.1.28>
2024-11-24 13:16:03 -0600

    paginado_compra_venta

--- appcompra/urls.py
+++ appcompra/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('compra_1', views.compra_1, name = 'compra_1' ),
     path('venta_1', views.venta_1, name = 'venta_1'),
     path('cv', views.cv, name= 'cv'),
+    path('com_ven', views.com_ven, name= 'com_ven'),
     ]
 
