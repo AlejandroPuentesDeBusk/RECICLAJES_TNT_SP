951a3feac05da3a74bcbd3274003e3590fc0a623
Alejandro Puentes de busk <alejandro@192.168.1.28>
2024-11-24 23:22:19 -0600

    agregando cv y ajustes caja

--- app_CorteCaja/views.py
+++ app_CorteCaja/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth import get_user_model
-from mainapp.models import Material, Transaction
+from mainapp.models import Material, Transaction, Day_Report
 from django.utils import timezone
 
 # Create your views here.
@@ -9,5 +9,10 @@ from django.utils import timezone
 
 #@login_required
 def ajustes_1(request):
-    return render(request, 'ajustes_caja/ajustes_1.html')
+    money= Day_Report.objects.latest('Day')
+    final_money= money.Final_Money
 
+    box_money = {'final_money': final_money}
+
+
+    return render(request, 'ajustes_1.html',{'box_money': box_money})
