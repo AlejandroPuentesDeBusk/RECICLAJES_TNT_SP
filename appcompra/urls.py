68a62079757827108bc2a60016f54cf5bb289036
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2024-11-24 23:26:02 -0600

    dj a

--- appcompra/urls.py
+++ appcompra/urls.py
@@ -7,10 +7,6 @@ from django.conf.urls import handler404
 
 
 urlpatterns = [
-    path('compra_venta', views.menu_cv, name='compra_venta'),
-    path('compra_1', views.compra_1, name = 'compra_1' ),
-    path('venta_1', views.venta_1, name = 'venta_1'),
     path('cv', views.cv, name= 'cv'),
-    path('com_ven', views.com_ven, name= 'com_ven'),
     ]
 
