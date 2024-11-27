95807f10e0e237f08f2c68fd4890aeb1b6be7572
Alejandro Puentes de busk <alejandro@192.168.1.28>
2024-11-26 19:41:35 -0600

    caja_muestra_inv_gas_hoy

--- app_CorteCaja/views.py
+++ app_CorteCaja/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth import get_user_model
 from mainapp.models import Material, Transaction, Day_Report
 from django.utils import timezone
+from datetime import datetime, timedelta
+
 
 # Create your views here.
 
@@ -11,9 +13,23 @@ from django.utils import timezone
 def ajustes_1(request):
 
     today = timezone.now().date()
-    money = Day_Report.objects.filter(Day=today)
+    #para que solo sean de hoy
+    start_of_day = timezone.make_aware(datetime.combine(today, datetime.min.time()))
+    end_of_day = timezone.make_aware(datetime.combine(today, datetime.max.time()))
+
     hora_actual = timezone.now()
 
+
+    in_ga = Transaction.objects.filter(
+        Date__range=(start_of_day, end_of_day),
+        Transaction_Type__in=["INVESTMENT", "EXPENSE"]
+    )
+
+
+    money = Day_Report.objects.filter(Day=today)
+
+
+    #Dinero del dia de hoy
     money_all=Day_Report.objects.all()
     
     # Consolidar el dinero final (opcional)
@@ -21,4 +37,4 @@ def ajustes_1(request):
 
     box_money = {'final_money': final_money}
     
-    return render(request, 'ajustes_1.html', {'box_money': box_money, 'money_all': money_all, 'hora_actual':hora_actual})
+    return render(request, 'ajustes_1.html', {'box_money': box_money, 'money_all': money_all, 'hora_actual':hora_actual, 'in_ga':in_ga})
