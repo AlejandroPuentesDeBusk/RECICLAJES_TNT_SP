c652ca0a036b796c2750c9b475ddf1aaaa64259b
Alejandro Puentes de busk <alejandro@192.168.1.28>
2024-11-27 18:38:59 -0600

    stock_materiales

--- app_Dashboard/views.py
+++ app_Dashboard/views.py
@@ -1,29 +1,30 @@
 from django.utils.timezone import localtime, now
 from django.shortcuts import render
-from mainapp.models import Transaction
+from mainapp.models import Transaction_Details, Material
 from django.core.paginator import Paginator
 
 def dashboard(request):
-
-
-    #asi emparejamos las fechas, que luego se desfasa, time zone es la buena, la otra da mal tiempo
-
     today_start = localtime(now()).replace(hour=0, minute=0, second=0, microsecond=0)
     today_end = localtime(now()).replace(hour=23, minute=59, second=59, microsecond=999999)
 
-    transactions = Transaction.objects.filter(
-        Date__range=(today_start, today_end),
-        Transaction_Type__in=['COMPRA', 'VENTA']  
-    )
+    # Filtrar detalles de transacciones del día actual
+    transaction_details = Transaction_Details.objects.filter(
+        Transaction__Date__range=(today_start, today_end),
+        Transaction__Transaction_Type__in=['COMPRA', 'VENTA']
+    ).select_related('Material', 'Transaction')  # Optimizamos consultas
 
-    #esto pal paginado
-    materials_p = Paginator(transactions, 10)
-    page_numb = request.GET.get('page')
-    show_page = materials_p.get_page(page_numb)
+    # Paginación
+    paginator = Paginator(transaction_details, 5)
+    page_number = request.GET.get('page')
+    show_page = paginator.get_page(page_number)
 
+    # Obtener todos los materiales
+    materials = Material.objects.all()
 
     context = {
+        'show_page': show_page,
         'today': today_start.date(),
-        'show_page':show_page
+        'materials': materials,
     }
     return render(request, 'dash.html', context)
+
