4c44085a51cda91a79fdf2dfa1b450c99c251eea
Alejandro Puentes de busk <alejandro@192.168.1.28>
2024-11-28 00:35:59 -0600

    hora dash

--- app_Dashboard/views.py
+++ app_Dashboard/views.py
@@ -7,10 +7,13 @@ def dashboard(request):
     today_start = localtime(now()).replace(hour=0, minute=0, second=0, microsecond=0)
     today_end = localtime(now()).replace(hour=23, minute=59, second=59, microsecond=999999)
 
+    print(f"Rango: {today_start} - {today_end}")
+
+
     # Filtrar detalles de transacciones del día actual
     transaction_details = Transaction_Details.objects.filter(
         Transaction__Date__range=(today_start, today_end),
-        Transaction__Transaction_Type__in=['COMPRA', 'VENTA']
+        Transaction__Transaction_Type__in=['PURACHASE', 'SELL']
     ).select_related('Material', 'Transaction')  # Optimizamos consultas
 
     # Paginación de transacciones
