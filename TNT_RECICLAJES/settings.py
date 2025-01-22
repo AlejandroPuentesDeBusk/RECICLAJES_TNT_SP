19601dacc949624e3e5f6b416fdfa6cff0f002e8
Alejandro Puentes de busk <alejandro@192.168.1.21>
2025-01-21 23:26:38 -0600

    FLASE

--- TNT_RECICLAJES/settings.py
+++ TNT_RECICLAJES/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9aiv#cynfql6hl6)vyv6dfulr46v-36g#rh8(6zyck@44*&-0g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
